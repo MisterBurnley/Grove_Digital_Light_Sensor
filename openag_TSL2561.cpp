f6c3adfc20d976f9a6c6008cb5b7a8b958dceb6f
john <johnburnle@aol.com>
2016-09-05 18:23:14 +0100

    Update openag_TSL2561.cpp

--- openag_TSL2561.cpp
+++ openag_TSL2561.cpp
@@ -1,1 +1,16 @@
+#include "openag_TSL2561.h"
 
+//set the i2c address
+
+//turn sensor on then output working message and init time
+void TSL2561::begin() {
+  TSL2561.init();  
+}
+
+// read from the sensor and output lux reading
+void TSL2561::update() {
+  uint32_t curr_time = millis();
+  Wire.begin();
+  TSL2561.readVisibleLux();
+  
+}
