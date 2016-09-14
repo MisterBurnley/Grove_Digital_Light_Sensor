a3d0104700e4d2d0815ff04f9b9a41de61d62d1b
john <johnburnle@aol.com>
2016-09-14 14:04:01 +0100

    Update openag_TSL2561.cpp

--- openag_TSL2561.cpp
+++ openag_TSL2561.cpp
@@ -5,14 +5,14 @@
 
 //turn sensor on 
 void TSL2561::begin() {
-  TSL2561.init();  
+  _sensor.init();  
 }
 
 // read from the sensor and output lux reading
 void TSL2561::update() {
 
-bool TSL2561::get_light_illuminance(std_msgs::Int32 &msg) {
-  msg.data = (TSL2561.readVisibleLux());
+bool TSL2561::get_light_illuminance(std_msgs::Int64 &msg) {
+  msg.data = _sensor.readVisibleLux();
   bool res = _send_light_illuminance;
   _send_light_illuminance = false;
   return res;
