1c9e84738f4de2418e0b66c91d4fa42ab80eefd4
john <johnburnle@aol.com>
2016-09-14 14:09:04 +0100

    Update openag_TSL2561.cpp

--- openag_TSL2561.cpp
+++ openag_TSL2561.cpp
@@ -1,17 +1,17 @@
-#include "openag_TSL2561.h"
+#include "openag_LIT2561.h"
 
 //set the i2c address
 #define TSL2561_Address 0*29
 
 //turn sensor on 
-void TSL2561::begin() {
+void LIT2561::begin() {
   _sensor.init();  
 }
 
 // read from the sensor and output lux reading
-void TSL2561::update() {
+void LIT2561::update() {
 
-bool TSL2561::get_light_illuminance(std_msgs::Int64 &msg) {
+bool LIT2561::get_light_illuminance(std_msgs::Int64 &msg) {
   msg.data = _sensor.readVisibleLux();
   bool res = _send_light_illuminance;
   _send_light_illuminance = false;
