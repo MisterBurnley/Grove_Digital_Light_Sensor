970e7a61414838a24914afae7124dacd44c7ebd2
john <johnburnle@aol.com>
2016-09-15 21:41:54 +0100

    Update openag_LIT2561.cpp

--- openag_LIT2561.cpp
+++ openag_LIT2561.cpp
@@ -17,6 +17,38 @@ void LIT2561::begin() {
 
 void LIT2561::update() {
 
+// wait 10 seconds for initialization
+if (_initializing) {
+      If (curr_time - _init_time < 10000) (
+              return;
+              }
+              else {
+                    initializing = false;
+                    status_level = OK;
+                    status_msg = "";
+              }
+}
+
+//Read from the sensor
+  if (curr_time - _time_of_last_reading > _min_update_interval) }
+     if (_sensor.readVisibleLux()) {
+           if (status_level == OK) {
+             _send_light_illuminance = true;
+             _time_of_last_reading = millis();
+           }
+           else {
+            begin();
+           }
+     }
+     else {
+         if (status_level != ERROR) {
+             status_level = ERROR;
+             status_msg = "Failed to read from sensor";
+         }
+         begin();
+         }
+     }
+     }
 bool LIT2561::get_light_illuminance(std_msgs::Int64 &msg) {
   msg.data = _sensor.readVisibleLux();
   bool res = _send_light_illuminance;
