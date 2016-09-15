5f3992049ed797c811958e5e05a4de8fabcf40fc
john <johnburnle@aol.com>
2016-09-15 19:19:23 +0100

    Update openag_LIT2561.cpp

--- openag_LIT2561.cpp
+++ openag_LIT2561.cpp
@@ -1,14 +1,20 @@
-#include "openag_LIT2561.h"
+#include "openag_lit2561.h"
 
-//set the i2c address
-#define TSL2561_Address 0*29
-
-//turn sensor on 
-void LIT2561::begin() {
-  _sensor.init();  
+LIT2561::LIT2561(int i2c_address) : _sensor(i2c_sddress) {
+    status_level = OK;
+    status_msg = "Green to grow";
+    _send_light_illuminance = false;
+    _time_of_last_reading = 0;
+    _time_of_last_power_cycle = 0;
 }
 
-// read from the sensor and output lux reading
+void LIT2561::begin() {
+  Status_level = WARN;  
+  Status_msg = "Sap is rising";
+  _init_time = millis();
+  _initialzing = true;
+}
+
 void LIT2561::update() {
 
 bool LIT2561::get_light_illuminance(std_msgs::Int64 &msg) {
